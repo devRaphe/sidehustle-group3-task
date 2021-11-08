33efc77efe0e020c8e1b40ab183910f4dca63d4f
Ezekiel Etiabasi Raphael <72768624+eezekiel@users.noreply.github.com>
2021-11-08 20:35:54 +0100

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:sidehustle_biz_card/src/bizcard_home_view/bizcard_home_view.dart
 
 void main() {
   runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: BizcardView(),
   ));
 }
